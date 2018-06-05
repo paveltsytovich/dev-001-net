cce5e656eba55848bf6812f6c0d11566138690c0
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 15:10:01 +0300

    Add FlatWithNoolite.cs

---
+++ FactoryMethod/FlatWithNoolite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class FlatWithNoolite : Application
+    {
+        protected override ICinema CreateCinema()
+        {
+            IRelay projector, lamp1;
+            IEngine engine1;
+            projector = new RelayNoolite();
+            lamp1 = new RelayNoolite();
+            engine1 = new StepMotor();
+            return new CinemaFlat(lamp1, projector, engine1);
+        }
+    }
+}
