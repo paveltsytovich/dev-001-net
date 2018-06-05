fa7220bca28ed1e0a76796092f12ca329321ce82
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 17:06:47 +0300

    Add factory implementation

---
+++ Singleton/SmartServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class SmartServiceFactory : ISmartServiceFactory
+    {
+        public IRelay CreateRelay()
+        {
+            return new Relay();
+        }
+
+        public IRGB CreateRGB()
+        {
+            return new RGB();
+        }
+    }
+}
