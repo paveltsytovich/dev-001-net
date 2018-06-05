0f9ec55f55da86be6137638c85eba664c95845f1
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 17:10:17 +0300

    Add SmartServiceCreator

---
+++ Singleton/SmartServiceCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SmartServiceCreator
+    {
+        private static ISmartServiceFactory instance;
+
+        public static ISmartServiceFactory Instance
+        {
+            get { return instance; }
+        }
+
+        static SmartServiceCreator()
+        {
+            instance = new SmartServiceFactory();
+        }
+    }
+}
