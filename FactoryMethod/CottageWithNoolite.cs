f0851e0e37b4a3cd18bc7e1ab1d82deba39ee618
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 15:12:36 +0300

    Add CottageWithNoolite.cs

---
+++ FactoryMethod/CottageWithNoolite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class CottageWithNoolite : Application
+    {
+        protected override ICinema CreateCinema()
+        {
+
+            IRelay projector, lamp1, lamp2;
+            IEngine engine1, engine2;
+            projector = new RelayNoolite();
+            lamp1 = new RelayNoolite();
+            lamp2 = new RelayNoolite();
+            engine1 = new StepMotor();
+            engine2 = new StepMotor();
+            return new CinemaCottadge(projector, lamp1, lamp2,engine1, engine2);
+        }
+    }
+}
