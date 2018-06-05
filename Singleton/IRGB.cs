7b66b4f8609bfce738ece8b8da3c666129573f62
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 16:59:47 +0300

    Add Singleton project

---
+++ Singleton/IRGB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+     interface IRGB
+     {
+         void SetColor(int r, int b, int c);
+         void SetBrightness(int brightness);
+     }
+}
