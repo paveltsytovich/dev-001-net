bd84264973036c841eb8d73392fab666e8c15df5
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 17:02:47 +0300

    Add reallization for Relay and RGB

---
+++ Singleton/RGB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class RGB: IRGB
+    {
+        public void SetColor(int r, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBrightness(int brightness)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Singleton/Relay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Relay : IRelay
+    {
+        public void On()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Off()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
