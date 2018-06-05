356d98f26d2d9886a5ff70828f6b71ab7775ccc2
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-05 15:01:34 +0300

    Add FactoryMethod projector

---
+++ FactoryMethod/CinemaCottadge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class CinemaCottadge : ICinema
+    {
+        private IRelay projector;
+        private IRelay lampInRoom, lampInHall;
+        private IEngine wall, windowBlindes;
+        public CinemaCottadge(IRelay p, IRelay lr,
+            IRelay lh, IEngine w, IEngine wb)
+        {
+            projector = p;
+            lampInRoom = lr;
+            lampInHall = lh;
+            wall = w;
+            windowBlindes = wb;
+        }
+        public void Show()
+        {
+            lampInHall.Off();
+            lampInRoom.On();
+            wall.turnForward();
+            windowBlindes.turnForward();
+            projector.On();
+        }
+
+        public void Stop()
+        {
+            lampInHall.On();
+            windowBlindes.turnBack();
+        }
+
+        public void Off()
+        {
+            projector.Off();
+            lampInHall.On();
+            lampInRoom.On();
+            wall.turnBack();
+            windowBlindes.turnBack();
+        }
+    }
+}
---
+++ FactoryMethod/RelayZWave.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class RelayZWave : IRelay
+    {
+        public void On()
+        {
+            Console.WriteLine("RelayZWave::On");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("RelayZWave::Off");
+        }
+    }
+}
