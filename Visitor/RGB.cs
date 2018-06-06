2197a5f6a90366ee54c78b40b0ba7a4fa2588bee
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-06 16:49:21 +0300

    Add RGB

---
+++ Visitor/RGB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class RGB : IRgb
+    {
+        public void setColor(int r, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
