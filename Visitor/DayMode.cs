670b36560bbe67a24bc7b587b0e0cb4719db88be
teacher112 <teacher112@PTC-DEV1N-140>
2018-06-06 16:54:37 +0300

    Implement IVisitor

---
+++ Visitor/DayMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class DayMode : IVisitor
+    {
+        public void SetRelay(Relay relay)
+        {
+            relay.Off();
+        }
+
+        public void SetRGB(RGB rgb)
+        {
+            rgb.setColor(0,0,0);
+        }
+    }
+}
---
+++ Visitor/NightMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class NightMode : IVisitor
+    {
+        public void SetRelay(Relay relay)
+        {
+            relay.On();
+        }
+
+        public void SetRGB(RGB rgb)
+        {
+            rgb.setColor(0,255,0);
+        }
+    }
+}
